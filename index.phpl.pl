3337a69036bb369f504d02ab32117846dc77f5f9
kronshevskiy_mac <kronshevskiy@gmail.com>
2024-08-30 00:54:48 +0300

    test

---
+++ index.phpl.pl
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--ГОЛОВА-->
+    <head>
+        <div style="height: 100px">
+            <div style="background-color: rgb(39, 38, 38);">
+                <button style="margin-left: 6px;background-color:aqua">TEST 1</button>
+                <button style="margin: 3px;background-color:green">TEST 2</button>
+                <button style="margin: 3px;background-color:orange">TEST 3</button>
+                <button style="margin: 3px;background-color:purple">TEST 4</button>
+
+            </div>
+        </div>
+    </head>
+
+    <!--КОНТЕНТ-->
+    <div>
+        <!--САЙДБАР-->
+        <div style"width: 30%; background-color:black ;>
+            <?php echo '<p>Привет, мир!</p>'; ?>
+        </body>
+        </div>
+    </div>
+                  
+</body>
+</html>
